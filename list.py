91eb1411a6290b11e02a392eb5558597ccf27782
Aniketch45 <aniketchafekar2004@gmail.com>
2025-10-08 09:14:53 +0530

    leraned list declaring accessing

---
+++ list.py
@@ -0,0 +1,20 @@
+# l1=eval(input("Enter a list Elements :")) //eval() automatically detects data list/tuple/string/list/dict
+# print(l1)
+# print(type(l1))
+
+
+l2=[30,60,'Aniket',10.60,True,60]
+# print(l2)
+# print(type(l2))
+
+#print using for loop
+# i=len(l2)
+# for i in l2:
+#     print(i)
+
+# using while
+len=len(l2)
+i=0
+while i<len:
+    print(l2[i])
+    i+=1
---
+++ string2.py
@@ -0,0 +1,57 @@
+#6.replacemant op {}
+name="Tom"
+age=40
+sal=35000
+# #print("{0} is earning Rs.{1} and age is {2}".format(name,age,sal))
+print("{} is earning rs.{} and age is {}".format(name,age,sal))
+# #print("{x} is earning Rs.{y} and age is {z}".format(z=age,x=name,y=sal))
+# print(f"{name} is earning rs.{sal} and age is {age}")
+
+
+#5.isalnum(),isalpha(),isdigit(),islower(),istitle(),isspace()
+
+# s1="    "
+#print(s1.isalnum())
+# print(s1.isalpha())
+# print(s1.isdigit())
+# print(s1.islower())
+# print(s1.istitle())
+#print(s1.isspace())
+
+
+#4.startswith(),endswith()
+# str="I Am Learning python basic funDaMentals"
+# print(str.startswith('I A'))
+# print(str.endswith('I'))
+# print(str.endswith('als'))
+
+#3.upper(),lower(),swapcase(),title(),capitalize()
+# str="I Am Learning python basic funDaMentals"
+# s1=str.upper()
+# s1=str.lower()
+# s1=str.swapcase()
+# s1=str.title()
+# s1=str.capitalize()
+# print(s1)
+
+
+
+#2.join()
+
+'''l1=["Hi","How","are","uuu"]
+l=' '.join(l1)
+print(l)'''
+
+"""str="Learningpythonbasics"
+a=' '.join(str)
+print(a)"""
+
+
+# d="12/10/2012"
+# date=d.split("/")
+# print(date)
+
+# s1="Helo My Name is Aniket"
+# s=s1.split()
+# print(s)
+

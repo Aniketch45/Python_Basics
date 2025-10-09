306c822ccd57bec8cd333be3ef9476b4ce684c4f
Aniketch45 <aniketchafekar2004@gmail.com>
2025-10-09 10:39:16 +0530

    learned module accessing in a local file

--- moduleex.py
+++ moduleex.py
@@ -1,5 +1,24 @@
-from modules import module1 as f
+# from modules import module1 as f
 
-print(f.x)
-print(f.y)
-f.prod(20,30)
+# print(f.x)
+# print(f.y)
+# f.prod(20,30)
+
+# from modules import module1 
+# module1.prod(10,20)
+
+# from module2 import prod,x
+# prod(20,30)
+# print(x)
+# print(y) we cannot access 
+
+# from module2 import *
+# prod(20,67)
+# print(y)
+# print(x)
+
+# import module2 as m
+# m.prod(3,5)
+
+from module2 import prod as p
+p(3,4)

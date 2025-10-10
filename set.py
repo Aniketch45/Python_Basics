75966d386f7fe795583f58feac4c12448e8c0636
Aniketch45 <aniketchafekar2004@gmail.com>
2025-10-10 09:55:15 +0530

    learned and practice set intialize,built in fun

---
+++ set.py
@@ -0,0 +1,63 @@
+# s1=eval(input("Enter values : "))
+# print(s1)
+
+# l1=[10,20,30,22]
+# s=set(l1)
+# print(s)
+# print(type(s))
+
+#declare
+# s={} #dict
+# s=set()
+# print(s)
+# s.add(50)
+# print(type(s))
+# print(s)
+
+#intialize
+# s={10,40,20,"Aniket",True,40,40}
+# print(s)
+# print(type(s))
+
+# print(s[3]) //error
+
+
+#buit-in methods add(),update()
+
+# s1={10,20,30,50}
+# s1.add(44)
+# print(s1)
+# print(type(s1))
+
+# s1.update([22,33,4,66])
+# print(s1)
+
+# l=[1,20,6,7]
+# r=range(1,6)
+# s1.update(l,r)
+# print(s1)
+
+# s1={10,20,30}
+# s2=s1 #aliasing
+# s2=s1.copy() #clone
+# s1.add(70)
+# print(s1)
+# print(s2)
+
+# s1={}
+# s1.pop()#error
+
+s1={66,55,44,33,66,88,99}
+s1.pop()
+print(s1)
+
+s1.remove(99) #show error if element not present
+print(s1)
+
+s1.discard(100) #doent error show if eleemnt not present
+
+s1.clear()
+print(s1)
+
+del s1
+print(s1)

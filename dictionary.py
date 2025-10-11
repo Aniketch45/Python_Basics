010a31709c2547cb82abfcb0b9f1c0f228447807
Aniketch45 <aniketchafekar2004@gmail.com>
2025-10-11 11:59:29 +0530

    dictionary creation and inti

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+d={'a':"aplause",'Id':17,'fname':"Aniket",34:"homeno"}
+print(d)
+print(type(d))
+
+d['Id']=200
+print(d)
+
+d['f']="fan"
+print(d)
--- set_operatins.py
+++ set_operatins.py
@@ -24,6 +24,17 @@ s2={40,50,60,90}
 
 #symmetric retuns all elemts that are diffrent in each
 # s1=s1.symmetric_difference(s2)
-s1.symmetric_difference_update(s2)
-print(s1^s2)
-print(s1)
+# s1.symmetric_difference_update(s2)
+# print(s1^s2)
+# print(s1)
+
+
+# s={i*i for i in range(0,51,2)}
+# print(s)
+
+#Q write a program to print vowels present in  the given  word?
+w=input("Enter a string : ")
+s=set(w)
+v={'a','e','i','o','u','A','E','I','O','U'}
+s.intersection_update(v)
+print("the vowel presnt in",w,"are",s)

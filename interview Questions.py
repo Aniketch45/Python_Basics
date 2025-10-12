75a3990850902eacccfb95c2e3d2f80d08d19a40
Aniketch45 <aniketchafekar2004@gmail.com>
2025-10-12 23:09:02 +0530

    practice basic interviiew questions

---
+++ interview Questions.py
@@ -0,0 +1,24 @@
+# q1) interview Questions
+
+# comment->#
+# '''comment'''
+
+# q2) data types in python
+# ->there are many datatypes in pyhon i start with fundamental dattypes
+# 1)int,float,complex,boolean,string and 
+# complex datatypes those are
+# byte,bytearray,list,tuple,set,dicitonary,set,frozenset
+
+# q3) which are identity operators and expplain them.
+# -> is and is not
+# is operator is use to compare address of object if address is same returns True
+# is not operator is ude to compare address if is diffrent it returns true
+
+# q4) what is conditonal opreator 
+# ->first we check condition if its true then the block of ConnectionAbortedError
+
+# q5) expalin logical oprator
+# -> And, Or ,not
+# And returns true if both conditoins asre true
+# or returns true if any one condition is True
+# not return result opposite true->false false->true

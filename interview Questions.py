51cc2bab287c4ec59dad4ca44ce9a3e2a18ab294
Aniketch45 <aniketchafekar2004@gmail.com>
2025-10-12 23:59:29 +0530

    q

--- interview Questions.py
+++ interview Questions.py
@@ -19,6 +19,39 @@
 
 # q5) expalin logical oprator
 # -> And, Or ,not
-# And returns true if both conditoins asre true
-# or returns true if any one condition is True
-# not return result opposite true->false false->true
+# in and and or their are two expression left and right
+# in and operator if both the ecpressions are true it return true if anyone is false it returns false
+# in or operator if anyone of the expression is ture it returns true ,it return false if both are false
+# in not it reverse the expession if expression is true->false false->true
+
+# q6) How to take data from User
+# -> by using  input() function is a function which get data from the User
+# input function whatever data we take it takes into string datatype,
+# if you want to take another datatype you need to convert it,
+# for ex if you want to take integer from the user form input function u need to typecast with int function
+
+# otherwise we have eval function which will wvaluste according to datatype provided
+# ex;> eval(input("Enter data"))
+# if list give in [] brackets
+
+# q7)What is swapcase() function in python.
+# -> it is a string builtin function which converts uppercase to lowercase
+# lowercase to upperase
+
+# q8) what is diffrence between / and //
+# -> / (divison operator )use for divisionl -> after dividing it returns float value if we not want flaot we use
+#    // (floor divison) use for floor division ->returns int
+
+# q9)What does python is dynamically typed langauge means
+# -> you dont have to declare datatpe intially in Python
+#   whatever data you give the varaible forms that datatype that why python is dynamically type
+# 
+# q10)what is difference between mutable and immutable datatype
+# -> immutable once object is created you cannot make changes ex. tuple,int, float, string
+# mutable the once the object is created you can make changes ex. list,set dictionary
+
+# q11) Built-in methods of set
+# --> some built-in methods of set are add() update()
+# union() intersection() intersection_update()
+# symettricdifference() symettrcdiffrenece_update
+# difference() remove() discard() clear() pop()
